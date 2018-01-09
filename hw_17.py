4a682c59c54b83ee5cff1b75442ba78d23c280d0
Pavel <pasha9shinyaev@gmail.com>
2018-01-09 18:50:34 +0200

    Variable name changed to snake_style

--- hw_17.py
+++ hw_17.py
@@ -2,12 +2,12 @@ import math
 
 
 def solve_quadratic_equation(a, b, c): # returns 2 values: either 2 roots or 2 Nones
-    Discriminant = b**2 - 4 * a * c
-    if Discriminant > 0:
-        x1 = (-b + math.sqrt(Discriminant)) / (2 * a)
-        x2 = (-b - math.sqrt(Discriminant)) / (2 * a)
+    discriminant = b**2 - 4 * a * c
+    if discriminant > 0:
+        x1 = (-b + math.sqrt(discriminant)) / (2 * a)
+        x2 = (-b - math.sqrt(discriminant)) / (2 * a)
         return x1, x2
-    elif Discriminant == 0:
+    elif discriminant == 0:
         x1 = -b / (2 * a)
         x2 = None
         return x1, x2
