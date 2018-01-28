9486088529667e2cc358341b93b656cb141d71d5
Pavel <pasha9shinyaev@gmail.com>
2018-01-28 11:04:41 +0200

    Init commit

--- hw_1_5.py
+++ hw_1_5.py
@@ -5,11 +5,11 @@ def closer_to_ten(number1, number2):
     delta1 = abs(10 - number1)
     delta2 = abs(10 - number2)
     if delta1 < delta2:
-        return 'Число %d ближе к 10 чем число %d' % (number1, number2)
+        return number1
     elif delta2 < delta1:
-        return 'Число %d ближе к 10 чем число %d' % (number2, number1)
+        return number2
     elif delta2 == delta1:
-        return 'Число %d и %d находятся на одинаковом расстоянии от 10' % (number2, number1)
+        return number1
 
 
 print(closer_to_ten(int(input()), int(input())))
